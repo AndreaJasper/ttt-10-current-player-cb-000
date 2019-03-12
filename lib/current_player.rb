8de1619c1bb88b5aad00ccc116c26f8d6873a17c
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-03-12 18:40:22 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,22 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+
+def turn_count(board)
+  count = 0
+  board.each do |turn|
+    if turn == "X" || turn == "O"
+      counter += 1
+      puts "#{counter}"
+    end
+  counter
+  end
+end
+
+
+def current_player(board)
+  if turn_count % 2
+    puts "X"
+  else
+    puts "O"
+  end
+end
